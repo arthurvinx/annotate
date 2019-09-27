a34d2db63293a1e7618286a8769dc34728045c89
Diego Morais <arthur.vinx@gmail.com>
2019-09-27 15:34:26 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name = 'annotate',
     author = 'Diego Morais',
-    author_email = "arthur.vinx@gmail.com",
+    author_email = 'arthur.vinx@gmail.com',
     description = 'Annotate each query using the best alignment for which a mapping is known',
     license = 'GPL3',
     version = '1.1',
-    url = "https://github.com/arthurvinx/annotate",
-    packages = 'annotate'
+    url = 'https://github.com/arthurvinx/annotate',
+    install_requires = ['plyvel'],
+    packages = ['annotate']
 )
