03fc7c58ed310930f323b282a1b92657e0afae52
Diego Morais <arthur.vinx@gmail.com>
2019-09-27 13:32:43 -0700

    Add files via upload

--- annotate/__init__.py
+++ annotate/__init__.py
@@ -1,3 +1,1 @@
 '''Annotate each query using the best alignment for which a mapping is known'''
-
-__version__ = '1.1'
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup, find_packages
-from version import find_version
+from setuptools import setup
 
 setup(
     name = 'annotate',
     author = 'Diego Morais',
+    author_email = "arthur.vinx@gmail.com",
     description = 'Annotate each query using the best alignment for which a mapping is known',
     license = 'GPL3',
-    version = find_version('annotate', '__init__.py'),
-    packages = find_packages()
+    version = '1.1',
+    url = "https://github.com/arthurvinx/annotate",
+    packages = 'annotate'
 )
