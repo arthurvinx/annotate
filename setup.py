31c0ccf90eb3d59c277b3952c2c2121445a68de0
Diego Morais <arthur.vinx@gmail.com>
2019-10-15 11:59:32 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
     description = 'Annotate each query using the best alignment for which a mapping is known',
     license = 'GPL3',
     version = '1.1',
-    packages = ['annotate']
+    packages = ['annotate'],
+    scripts=['bin/annotate'],
+    install_requires=['plyvel']
 )
