786f3b4f6effb1822cc5b4c5780c5ca7ac9d3090
Diego Morais <arthur.vinx@gmail.com>
2021-05-12 18:11:51 -0300

    Create/Update levelDB using plyvel 1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email = 'arthur.vinx@gmail.com',
     description = 'Annotate each query using the best alignment for which a mapping is known',
     license = 'GPL3',
-    version = '1.1',
+    version = '1.2',
     packages = ['annotate'],
     scripts=['bin/annotate'],
     install_requires=['plyvel']
